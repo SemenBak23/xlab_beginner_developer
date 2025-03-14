d9da576ecbf23974a280049f4ad4ed8056fb017d
Semen Bakhurtsov <s.bakhurtsov@carx-tech.com>
2025-03-14 16:24:48 +0300

    ConsoleApp

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 7;
+            if (a < b)
+            {
+                Console.WriteLine($"Число {b} большу числа {a}");
+            }
+
+
+        }
+    }
+}
+
+
+
